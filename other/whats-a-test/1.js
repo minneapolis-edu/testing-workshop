5fa858eb84072474b601908cb6ce6c1e5d6228a7
Clara <minneapolis-edu@users.noreply.github.com>
2018-02-21 09:35:49 -0600

    Update 1.js

--- other/whats-a-test/1.js
+++ other/whats-a-test/1.js
@@ -6,7 +6,7 @@ Normally your tests aren't in the same file as your source code.
 
 So now take your tests from before and copy it in here.
 
-Then run this code with `node 0`
+Then run this code with `node 1`
 
 > Make sure you're in the right directory!
 
